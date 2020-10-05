6cb37712c691d7656bdd1efae3f12288f50d2cfd
vvuonghn <vvuonghn@gmail.com>
2020-10-05 11:59:00 +0700

    SSD300

---
+++ SSD_300/create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/vinai/vuonghn/Research/CV_courses/a-PyTorch-Tutorial-to-Object-Detection/VOCdevkit/VOC2007/',
+                      voc12_path='/vinai/vuonghn/Research/CV_courses/a-PyTorch-Tutorial-to-Object-Detection/VOCdevkit/VOC2012/',
+                      output_folder='./')
